75030ca15a54ea6985d984de5127fa1425492cec
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-12-19 15:13:39 +0100

    CORE-1313 Reading product option data provided by Publish and Sync

---
+++ src/Spryker/Client/ProductOptionStorage/ProductOptionStorageDependencyProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Client\ProductOptionStorage;
+
+use Spryker\Client\ProductOptionStorage\Dependency\Client\ProductOptionStorageToStorageBridge;
+use Spryker\Client\ProductOptionStorage\Dependency\Service\ProductOptionStorageToSynchronizationServiceBridge;
+use Spryker\Client\Kernel\AbstractDependencyProvider;
+use Spryker\Client\Kernel\Container;
+use Spryker\Shared\Kernel\Store;
+
+class ProductOptionStorageDependencyProvider extends AbstractDependencyProvider
+{
+
+    const CLIENT_STORAGE = 'CLIENT_STORAGE';
+    const SERVICE_SYNCHRONIZATION = 'SERVICE_SYNCHRONIZATION';
+    const STORE = 'STORE';
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    public function provideServiceLayerDependencies(Container $container)
+    {
+        $container = $this->addStorageClient($container);
+        $container = $this->addSynchronizationService($container);
+        $container = $this->addStore($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    protected function addStorageClient(Container $container): Container
+    {
+        $container[self::CLIENT_STORAGE] = function (Container $container) {
+            return new ProductOptionStorageToStorageBridge($container->getLocator()->storage()->client());
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    protected function addSynchronizationService(Container $container): Container
+    {
+        $container[self::SERVICE_SYNCHRONIZATION] = function (Container $container) {
+            return new ProductOptionStorageToSynchronizationServiceBridge($container->getLocator()->synchronization()->service());
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Client\Kernel\Container $container
+     *
+     * @return \Spryker\Client\Kernel\Container
+     */
+    protected function addStore(Container $container): Container
+    {
+        $container[self::STORE] = function () {
+            return Store::getInstance();
+        };
+
+        return $container;
+    }
+
+}
